261fc3bf7101eccbb6a0bd09943d83b1861557f9
Rodrigo Mendes <35048303+rodimendes@users.noreply.github.com>
2023-08-16 22:41:58 +0000

    adiciona scripts e

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>angelo mendes vieira pinto</h1>
+  return <h1>angelo mendes vieira pinto</h1>;
+}
+
+function Teste() {
+  console.log("Testando");
+}
+
+function Teste2() {
+  console.log("Tudo errado");
 }
 
 export default Home;
