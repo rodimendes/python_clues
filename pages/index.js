dbd016bba7b97fc6070573994e2fc4a71c5b8607
Rodrigo Mendes <35048303+rodimendes@users.noreply.github.com>
2023-08-20 22:00:00 +0000

    Homepage main objectives

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,4 @@ function Home() {
   return <h1>angelo mendes vieira pinto</h1>;
 }
 
-function Teste() {
-  console.log("Testando");
-}
-
-function Teste2() {
-  console.log("Tudo errado");
-}
-
 export default Home;
