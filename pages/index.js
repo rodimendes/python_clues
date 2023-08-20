5b77d54c65968a401d544e4b1679f41ea7d39608
Rodrigo Mendes <35048303+rodimendes@users.noreply.github.com>
2023-08-20 22:07:44 +0000

    Homepage main objectives3

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,11 @@ function Home() {
     "Curtos e objetivas ações em Python",
     "Título, curta explicação e bloco de código",
   ];
-  return (<h1>{frases[0]}</h1>), (<h1>{frases[1]}</h1>);
+  return (
+    <h1>
+      {frases[0]}, {frases[1]}
+    </h1>
+  );
 }
 
 export default Home;
