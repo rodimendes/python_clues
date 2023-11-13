98e52423678cee084b040c0c7a358ca2b527d6a3
Rodrigo Mendes <35048303+rodimendes@users.noreply.github.com>
2023-11-13 17:31:44 +0000

    Title and post title included

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
 function Home() {
-  var frases = [
+  var title = [
     "Curtos e objetivas ações em Python ou outras linguagens",
     "Título, curta explicação e bloco de código",
   ];
   return (
-    <h1>
-      {frases[0]}, {frases[1]}
-    </h1>
+    <><h1>
+      {title[0]}
+      {title[1]}
+    </h1><h2>
+        How to get the current timestamp?
+      </h2></>
   );
 }
 
