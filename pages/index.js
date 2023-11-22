de6132c0ce169630741fe5066ea2602369f041ae
Rodrigo Mendes <35048303+rodimendes@users.noreply.github.com>
2023-11-22 23:27:21 +0000

    add `jsconfig.json` file

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,3 @@
-function Home() {
-  var title = [
-    "Curtos e objetivas ações em Python ou outras linguagens",
-    "Título, curta explicação e bloco de código",
-  ];
-  return (
-    <><h1>
-      {title[0]}
-      {title[1]}
-    </h1><h2>
-        How to get the current timestamp?
-      </h2></>
-  );
-}
+import { Home } from "./Home";
 
 export default Home;
