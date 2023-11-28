77c6bf37a3e4f27a97d41b0c21665811c522a2a2
Rodrigo Mendes <35048303+rodimendes@users.noreply.github.com>
2023-11-28 17:47:05 +0000

    fix `package.json` file

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,16 @@
-import { Home } from "./Home";
+function Home() {
+  var title = [
+    "Curtos e objetivas ações em Python ou outras linguagens",
+    "Título, curta explicação e bloco de código",
+  ];
+  return (
+    <><h1>
+      {title[0]}
+      {title[1]}
+    </h1><h2>
+        How to get the current timestamp?
+      </h2></>
+  );
+}
 
 export default Home;
