7489d0074c0bfe8c291ee1108145508e4c612334
Rodrigo Mendes <35048303+rodimendes@users.noreply.github.com>
2023-12-26 23:33:07 +0000

    finish `api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,31 @@
 import database from "infra/database.js"
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;")
-  console.log(result);
-  response.status(200).json({status: "A API está rodando..."});
+  const updatedAt = new Date().toISOString();
+  const dbVersion = await database.query("SHOW server_version;");
+  const databaseVersion = dbVersion[0].server_version;
+  
+  const dbMaxConnections = await database.query("SHOW max_connections;");
+  const dbMaxConnectionsValue = dbMaxConnections[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const dbOpenedConnectionsResults = await database.query({
+    text: "SELECT count(*) FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName]
+  })
+  // const dbOpenedConnectionsResults = await database.query("SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'local_db';") Podemos usar o ::int para já vir do banco convertido.
+  const dbOpenedConnectionsValue = dbOpenedConnectionsResults[0].count
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersion,
+        max_connections: parseInt(dbMaxConnectionsValue),
+        opened_connections: parseInt(dbOpenedConnectionsValue),
+      }
+    }
+  });
 
 }
 
