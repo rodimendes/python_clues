f5344083c9dc7bf5dbdcd50c027dc90ffefb33aa
Rodrigo Mendes <35048303+rodimendes@users.noreply.github.com>
2024-10-12 20:59:54 +0000

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -8,14 +8,18 @@ beforeAll(async () => {
   await database.query("drop schema public cascade; create schema public");
 });
 
-test("GET to /api/v1/migrations should return 200.", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations");
+describe("GET /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving pending migrations", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/migrations");
 
-  expect(response.status).toBe(200);
+      expect(response.status).toBe(200);
 
-  const responseBody = await response.json();
-  // console.log(responseBody); //Prints the pending migration to be executed. Remember, we are in a dryRun environment. This console.log can be removed
+      const responseBody = await response.json();
+      // console.log(responseBody); //Prints the pending migration to be executed. Remember, we are in a dryRun environment. This console.log can be removed
 
-  expect(Array.isArray(responseBody)).toBe(true); // Checks if the responseBody is an array.
-  expect(responseBody.length).toBeGreaterThan(0);
+      expect(Array.isArray(responseBody)).toBe(true); // Checks if the responseBody is an array.
+      expect(responseBody.length).toBeGreaterThan(0);
+    });
+  });
 });
